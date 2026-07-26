ff905b9887f87e4c0bc957ec659f6e4221d425e2
aharsha1 <aharshavardhan2593@gmail.com>
2026-07-26 11:52:38 +0530

    Use Streamlit secrets for API key

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-import os
 import streamlit as st
-from dotenv import load_dotenv
 from groq import Groq
 
 
-# Load API key
-load_dotenv()
-
 client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
+    api_key=st.secrets["GROQ_API_KEY"]
 )
 
 
